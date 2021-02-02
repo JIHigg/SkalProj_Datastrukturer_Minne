750b8eac32623660df1db876fa18bf31e613c2ea
higginsdude@gmail.com <higginsdude@gmail.com>
2021-02-02 15:05:27 +0100

    ExamineList Method

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SkalProj_Datastrukturer_Minne
 {
     class Program
-    {
+    {      
         /// <summary>
         /// The main method, vill handle the menues for the program
         /// </summary>
         /// <param name="args"></param>
+        /// 
+
+        //F.1) Stacken är ordentligt ordnade i minnet i en "First in Last out" -ordning.
+        //     Det är effektivt organiserat och data kastas när operationen är klar. 
+        //     Heapen är en mindre organiserad cache av data, med referens för att hitta 
+        //     allt snabbt. Den använder mer minne, men är mer flexibel.
+        //F.2) Value Types är data som lagras direkt i minnet. Reference Types är pointers 
+        //     som riktar sig till data.
+        //F.3) Den första exampel här x och y som två variabler till två olika datapunkter.
+        //     Först tilldelas y till x, sedan tilldelas y ett annat Value. X förblir tilldelat
+        //     samma Value. 
+        //     Den andra exampel här både x och y blir objekt som refererar till samma data.
+        //     När data modifieras med y referensar x till samma modifiereade data.
         static void Main()
         {
 
@@ -33,6 +47,15 @@ namespace SkalProj_Datastrukturer_Minne
                 {
                     case '1':
                         ExamineList();
+                        //F.2) Listans kapacitet ökar när antalet fyllda utrymmen matchar
+                        //     den aktuella kapaciteten för utrymmen.
+                        //F.3) Listans kapacitet ökar först till fyra och fördubblas varje gång.
+                        //F.4) Eftersom listan vill säkerställa att det finns tillräckligt med 
+                        //     minne för objekten flyttas den automatiskt till en array med dubblet
+                        //     så stor storlek.
+                        //F.5) Listans kapacitet minskar inte när element tas bort ur listan.
+                        //F.6) När du vet exakt hur många items du har, är det då fördelaktigt att
+                        //     använda en Array instället för en lista.
                         break;
                     case '2':
                         ExamineQueue();
@@ -78,6 +101,63 @@ namespace SkalProj_Datastrukturer_Minne
             //string value = input.substring(1);
 
             //switch(nav){...}
+
+            List<string> thisList = new List<string>();
+            bool open = true;
+
+            while (open)//Allows method to repeat until closed with '0'
+            {
+                Console.WriteLine("Make an addition or subtraction to the list" +
+                                    " beginning with either '+' or '-' [Enter 0 to exit]: ");
+                string input = Console.ReadLine();
+                string value = input.Substring(1);
+                char nav = input[0];
+
+                switch (nav)
+                {
+                    case '+':
+                        try
+                        {
+                            thisList.Add(value);
+                        }
+                        catch (Exception)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Invalid input. Please try again.");
+                        }
+
+                            Console.WriteLine($"The list has added {value} and has now grown by: " +
+                                thisList.Capacity.ToString());
+                            Console.WriteLine("List count: "+ thisList.Count.ToString());
+                            Console.WriteLine("List capacity: "+ thisList.Capacity.ToString());
+                        break;
+                    case '-':
+                        try
+                        {
+                           if( thisList.Remove(value))
+                            {
+                            Console.WriteLine($"The list has removed {value} and has now reduced to: " +
+                                thisList.Capacity.ToString());
+                            }
+                           else
+                                Console.WriteLine("Unable to remove from List.");
+                        }
+                        catch (Exception)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Please try again.");
+                        }
+                            Console.WriteLine("List count: " + thisList.Count.ToString());
+                            Console.WriteLine("List capacity: " + thisList.Capacity.ToString());
+                        break;
+                    case '0':
+                        open = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please start your message with a '+' or '-'");
+                        break;
+                }
+            }
         }
 
         /// <summary>
